7e2894daf76e89ba0b49ad9df61004e8665a1c24
awaw00 <awaw0618@outlook.com>
2017-03-07 17:02:30 +0800

    save

---
+++ src/reducers/user.js
@@ -0,0 +1,21 @@
+export function dataSource (state = [], action) {
+  switch (action.type) {
+
+    default:
+      return state;
+  }
+}
+
+export function formValues (state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export function editTarget (state = null, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
